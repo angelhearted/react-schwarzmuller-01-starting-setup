372f0a5d12c7930a195124de85f8e15c9b0ee616
Nikita Skrebets <angelhearted@ya.ru>
2021-05-23 05:22:05 +0300

    Add Expense creation form and Filter (both expectedly not working currently)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
 
 export default function Expenses(props) {
+  const [year, setYear] = useState("2020");
+  const handleSelectYear = (year) => {
+    setYear(year);
+    console.log(year);
+  };
+
   return (
     <Card className="expenses">
+      <ExpenseFilter year={year} onSelectYear={handleSelectYear} />
       <ExpenseItem {...props.expenses[0]} />
       <ExpenseItem {...props.expenses[1]} />
       <ExpenseItem {...props.expenses[2]} />
